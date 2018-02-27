dc2b75dec0950685cb12731dcec4bcf50fff3240
FawziehA <35780330+FawziehA@users.noreply.github.com>
2018-02-27 12:00:19 -0600

    Update Simulator.java

--- src/Simulator.java
+++ src/Simulator.java
@@ -1,4 +1,53 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-public class Simulator {
+public class Simulator{
+	ChronoTimer ct = new ChronoTimer();
+	boolean simulatorOn = true;
 
+	public Simulator(){
+		try(Scanner sc = new Scanner (new File("racers.txt"))){
+			while(sc.hasNextLine() && simulatorOn){
+				fileParser(sc.nextLine().trim().split(" "));
+			}
+		}
+		catch(FileNotFoundException e){
+			System.out.println("File not found!");
+		}
+	}
+
+	private void fileParser(String tokens[]) {
+		switch(tokens[1]){
+		case "POWER":
+		//	ct.power();
+			break;
+		case "NEWRUN":
+			//TODO
+			break;
+		case "TOG":
+			ct.toggle(tokens[2]);
+			break;
+		case "TRIG":
+			ct.trigger(tokens[2]);
+			break;
+		case "PRINT":
+			//TODO
+			break;
+		case "ENDRUN":
+			//TODO
+			break;
+		case "TIME":
+			//tokens[2] to set time 
+			break;
+		case "NUM":
+			
+			break;
+		case "EVENT":
+			break;
+		case "EXIT":
+			simulatorOn = false;
+			break;
+		}	
+	}
 }

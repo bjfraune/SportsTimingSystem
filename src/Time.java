877f1b54a544f11dcb55403d1907a9f20885bb3e
bsheps <35780702+bsheps@users.noreply.github.com>
2018-02-27 12:20:00 -0600

    Add files via upload

--- src/Time.java
+++ src/Time.java
@@ -1,4 +1,17 @@
+import java.time.format.DateTimeFormatter;
 
 public class Time {
-
+	private Time currentTime;
+	private final DateTimeFormatter formatTime=DateTimeFormatter.ofPattern("HH:mm:ss.SSS");;
+	
+	public void setTime(Time time){
+		currentTime = time;
+	}
+	public Time getTime(){
+		return currentTime;
+	}
+	public String time2formattedString(Time time){
+		return time.formatTime.toString();
+	}
+	
 }

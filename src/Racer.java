fdaaf6c904df39a953fdc948d4cbc48828117e6f
bsheps <35780702+bsheps@users.noreply.github.com>
2018-02-27 11:27:58 -0600

    Add files via upload

--- src/Racer.java
+++ src/Racer.java
@@ -1,4 +1,32 @@
-
+/**
+ * @author bshepard
+ * Racers has 3 fields: name, startTime and endTime
+ */
 public class Racer {
 
+	private String name;
+	private double startTime, endTime;
+	
+	public Racer(){
+		name = null;
+		startTime = endTime =-1;
+	}
+	
+	public void setName(String racername){
+		name = racername;
+	}
+	public void startRace(double time){
+		startTime = time;
+	}
+	public void finishRace(double time){
+		endTime = time;
+	}
+	public double timeResult(){
+		return endTime - startTime;
+	}
+	public String formattedResults(){
+		return name + " "+ Double.toString(endTime-startTime);
+	}
+	
+	
 }
